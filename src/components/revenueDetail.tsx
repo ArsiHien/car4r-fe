9955cc4588b355778a037d4797a47d773e69be14
Manh Cuong <124763973+Deathsml4@users.noreply.github.com>
2024-11-20 21:34:01 +0700

    v2

--- src/components/revenueDetail.tsx
+++ src/components/revenueDetail.tsx
@@ -4,8 +4,22 @@ const RevenueDetail = () => {
     
     return (
         <div>
-            <div>
-                
+            <div className="w-1/6 h-24 rounded-lg bg-white p-6 shadow-md grid grid-cols-5">
+                <span className="text-xs -mt-3 text-left text-slate-600">
+                    Total Revenue
+                </span>
+                <span className="text-xs -mt-3 text-left text-slate-600">
+                    Total Revenue
+                </span>
+                <span className="text-xs -mt-3 text-left text-slate-600">
+                    Total Revenue
+                </span>
+                <span className="text-xs -mt-3 text-left text-slate-600">
+                    Total Revenue
+                </span>
+                <span className="text-xs -mt-3 text-left text-slate-600">
+                    Total Revenue
+                </span>
             </div>
         </div>
     );
