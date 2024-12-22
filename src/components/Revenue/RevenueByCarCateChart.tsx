fce83db59275eeead6d5a9ca3d39f64424b2cc23
Manh Cuong <124763973+Deathsml4@users.noreply.github.com>
2024-12-23 00:14:29 +0700

    Change RevByCarCateChart
    Đổi thành 1 màu cố định

--- src/components/Revenue/RevenueByCarCateChart.tsx
+++ src/components/Revenue/RevenueByCarCateChart.tsx
@@ -32,15 +32,7 @@ const RevenueByCarCateChart: React.FC = () => {
                 {
                   label: "Revenue",
                   data: values,
-                  backgroundColor: [
-                    "rgba(255, 99, 132, 0.8)",
-                    "rgba(255, 159, 64, 0.8)",
-                    "rgba(255, 205, 86, 0.8)",
-                    "rgba(75, 192, 192, 0.8)",
-                    "rgba(54, 162, 235, 0.8)",
-                    "rgba(153, 102, 255, 0.8)",
-                    "rgba(201, 203, 207, 0.8)",
-                  ],
+                  backgroundColor: "rgba(75, 192, 192, 1)",
                   borderColor: "rgba(75, 192, 192, 1)",
                   borderWidth: 1,
                 },
