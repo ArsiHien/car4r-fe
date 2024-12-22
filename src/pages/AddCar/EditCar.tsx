726e9e1cbbf962945ff5a52e1ad56b9c4c2b61e3
Nguyễn Đức Hiển <125260143+ArsiHien@users.noreply.github.com>
2024-12-22 23:54:20 +0700

    Update EditCar.tsx

--- src/pages/AddCar/EditCar.tsx
+++ src/pages/AddCar/EditCar.tsx
@@ -4,7 +4,6 @@ import EditCarForm from "../../components/AddCar/EditCarForm";
 const EditCar = () => {
   const location = useLocation();
   const carCategory = location.state?.carCategory;
-  console.log("cate: ", carCategory);
 
   return (
     <>
